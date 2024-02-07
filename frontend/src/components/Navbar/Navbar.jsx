71e9932d7a673d1c4d274b8ecc3ef42cbdc22690
Dennis Peter <peterdennis573@gmail.com>
2024-02-07 16:04:50 +0300

    Handled logout

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../reduxx/userSlice";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.user?.user?.loggedIn);
 
   const displayMenu = () => {
     setShowMenu(!showMenu);
+  };
+
+  const handleLogout = () => {
+   
+    dispatch(logout());
   };
 
   return (
@@ -21,30 +27,45 @@ const Navbar = () => {
               E-Lear<span>ning</span>
             </a>
           </div>
-          <div className={showMenu ? "navigation__menu active" : "navigation__menu"}>
+          <div
+            className={
+              showMenu ? "navigation__menu active" : "navigation__menu"
+            }
+          >
             <ul className="flex">
               <li className="ulList">
                 <Link to="/home">HOME</Link>
               </li>
-              {isLoggedIn ? <>
-                
+              {isLoggedIn ? (
+                <>
                   <li className="ulList">
-                    <Link to="#" className="school_link">PRE PRIMARY</Link>
+                    <Link to="#" className="school_link">
+                      PRE PRY
+                    </Link>
                   </li>
                   <li className="ulList">
-                    <Link to="#"className="school_link">PRIMARY</Link>
+                    <Link to="#" className="school_link">
+                     PRY
+                    </Link>
                   </li>
-                
+
                   <li className="ulList">
-                    <Link to="#"className="school_link">JUNIOR SECONDARY</Link>
+                    <Link to="#" className="school_link">
+                    SNR SNDRY
+                    </Link>
                   </li>
                   <li className="ulList">
-                    <Link to="#"className="school_link">SENIOR SECONDARY</Link>
+                    <Link to="#" className="school_link">
+                      SNR SNDRY
+                    </Link>
                   </li>
                   <li className="ulList">
-                    <Link to="#"className="school_link">SENIOR</Link>
+                    <Link to="#" className="school_link" onClick={handleLogout}>
+                      LOGOUT
+                    </Link>
                   </li>
-                </> : (
+                </>
+              ) : (
                 <>
                   <li className="ulList">
                     <Link to="/login">LOGIN</Link>
