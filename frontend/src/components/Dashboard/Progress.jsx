39a955f55d25372390c79d5ee3d6e420a02271af
Dennis Peter <peterdennis573@gmail.com>
2024-03-22 19:28:14 +0300

    updates

--- frontend/src/components/Dashboard/Progress.jsx
+++ frontend/src/components/Dashboard/Progress.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-
+import React from "react";
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer"
+import "./progress.css"
 const Progress = () => {
-  return (
-    <div className='announcements'>
-        <Navbar/>
-        <Footer/>
-    </div>
-  )
-}
+  const generateRandomMarks = () => {
+    return Math.floor(Math.random() * 101); // Generates a random number between 0 and 100
+  };
 
-export default Progress
+  const subjects = [
+    "Social Studies",
+    "English",
+    "Mathematics",
+    "Kiswahili",
+    "Integrated Science",
+    "Business Studies",
+    "Agriculture",
+    "Pre-Technical",
+    "Religious Education",
+  ];
+  
+  const marks = subjects.map((subject) => generateRandomMarks());
+
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const data = {
+  labels: subjects,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      borderWidth: 1, // Set the width of the line to cover the entire width of the canvas
+      data:marks,
+    },
+  ],
+};
+  return (
+    <div className="progress">
+      <Navbar/>
+      <p className="title">SUBJECTS MARKS PERFORMANCE</p>
+      <Line data={data} />
+      <p className="title">STUDENT PERSONAL PERFORMANCE</p>
+      <Line data={data} />
+      <p className="title">CLASS PERFORMANCE</p>
+      <Line data={data} />
+      <p className="title">SCHOOL PERFORMANCE</p>
+      <Line data={data} />
+      <Footer/>
+    </div>
+  );
+};
+export default Progress;
