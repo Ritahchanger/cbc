74f0c15541206d3b9398d8456a17602eb2d4bdbc
Dennis Peter <peterdennis573@gmail.com>
2024-03-22 19:44:43 +0300

    Updates

--- frontend/src/components/Dashboard/Progress.jsx
+++ frontend/src/components/Dashboard/Progress.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Chart from "chart.js/auto";
-import { Line } from "react-chartjs-2";
+import { Line, Pie, Bar } from "react-chartjs-2";
 import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer"
-import "./progress.css"
+import Footer from "../Footer/Footer";
+import "./progress.css";
 const Progress = () => {
   const generateRandomMarks = () => {
     return Math.floor(Math.random() * 101); // Generates a random number between 0 and 100
@@ -20,34 +20,79 @@ const Progress = () => {
     "Pre-Technical",
     "Religious Education",
   ];
-  
-  const marks = subjects.map((subject) => generateRandomMarks());
 
-  const labels = ["January", "February", "March", "April", "May", "June"];
+  const marks = subjects.map((subject) => generateRandomMarks());
   const data = {
-  labels: subjects,
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      borderWidth: 1, // Set the width of the line to cover the entire width of the canvas
-      data:marks,
-    },
-  ],
-};
+    labels: subjects,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        borderWidth: 1,
+        data: marks,
+      },
+    ],
+  };
+
+
+
+  const pieColors = [
+    "rgb(255, 99, 132)",
+    "rgb(54, 162, 235)",
+    "rgb(255, 206, 86)",
+    "rgb(75, 192, 192)",
+    "rgb(153, 102, 255)",
+    "rgb(255, 159, 64)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(128, 128, 128)",
+  ];
+  
+  const pieData = {
+    labels: subjects,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: pieColors,
+        borderColor: pieColors,
+        borderWidth: 1,
+        data: marks,
+      },
+    ],
+  };
+  
+
+
+
+
   return (
     <div className="progress">
-      <Navbar/>
-      <p className="title">SUBJECTS MARKS PERFORMANCE</p>
-      <Line data={data} />
-      <p className="title">STUDENT PERSONAL PERFORMANCE</p>
-      <Line data={data} />
-      <p className="title">CLASS PERFORMANCE</p>
-      <Line data={data} />
-      <p className="title">SCHOOL PERFORMANCE</p>
-      <Line data={data} />
-      <Footer/>
+      <Navbar />
+      <div className="container">
+        <p className="title">SUBJECTS MARKS PERFORMANCE</p>
+        <Bar data={data} />
+        <p className="title">STUDENT PERSONAL PERFORMANCE</p>
+        <Line data={data} />
+        <p className="title">CLASS PERFORMANCE</p>
+
+        <Pie
+          data={pieData}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Users Gained between 2016-2020",
+              },
+            },
+          }}
+        />
+
+        <Line data={data} />
+        <p className="title">SCHOOL PERFORMANCE</p>
+        <Line data={data} />
+      </div>
+      <Footer />
     </div>
   );
 };
